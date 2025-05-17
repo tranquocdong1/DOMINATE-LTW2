f4acd5df15a618bc1d400d27728345151edd96a8
hoquanglanh <lanhvivu1245@gmail.com>
2025-05-17 13:54:32 +0700

    first

---
+++ src/main/java/donga/edu/example1/model/Member.java
@@ -0,0 +1,17 @@
+package donga.edu.example1.model;
+
+public class Member {
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
